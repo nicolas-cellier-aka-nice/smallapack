27b4eb74bc583bd27c77243d7c174ebc8f978712
nicolas.cellier.aka.nice <nicolas.cellier.aka.nice@gmail.com>
2015-07-31 13:13:27 +0000

    add code and project for generating a xerbla replacement library on macosx

---
+++ xerbla/macosx_10.6/xerbla.c
@@ -0,0 +1,38 @@
+/*
+// 2003 01 21 @NC
+// routine rapport d'erreur pour BLAS/LAPACK
+*
+*  -- LAPACK auxiliary routine (preliminary version) --
+*     Univ. of Tennessee, Univ. of California Berkeley, NAG Ltd.,
+*     Courant Institute, Argonne National Lab, and Rice University
+*     February 29, 1992
+*
+*  Purpose
+*  =======
+*
+*  XERBLA  is an error handler for the LAPACK routines.
+*  It is called by an LAPACK routine if an input parameter has an
+*  invalid value.  A message is printed and execution stops.
+*
+*  Installers may consider modifying the STOP statement in order to
+*  call system-specific exception-handling facilities.
+*
+*  Arguments
+*  =========
+*
+*  SRNAME  (input) CHARACTER*6
+*          The name of the routine which called XERBLA.
+*
+*  INFO    (input) INTEGER
+*          The position of the invalid parameter in the parameter list
+*          of the calling routine.
+*
+*/
+
+#include <stdio.h>
+
+void xerbla_(char srname[],int *info,int lsrname)
+{
+	fprintf(stderr," ** On entry to %6s parameter number %2d had an illegal value\n",srname,*info);
+}
+
